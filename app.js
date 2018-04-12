db0767f6ea21f4579eb49c17f363402be98c9cee
nkent <nkent@eecs.wsu.edu>
2018-04-11 19:16:44 -0700

    simple fix

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = 5000;
+var port = process.env.port || 5000;
 
 app.get('/',function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(port,function(){
-    console.log("App has started");
+    console.log("App has started with port: ", port);
 });
 
 var messageHistory = []
