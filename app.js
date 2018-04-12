ddd42e19bd55908baf746bc6852badc19d9c32c0
nkent <nkent@eecs.wsu.edu>
2018-04-11 17:56:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = 5000;
+
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port,function(){
+    console.log("App has started");
+});
+
+var messageHistory = []
+
+io.sockets.on('connection', function(socket){
+    console.log("new user connected with id <" + socket.id + '>');
+    socket.emit('data', messageHistory);
+    socket.join('private_room', () => {
+        let rooms = Object.keys(socket.rooms);
+        console.log(rooms); // [ <socket.id>, 'private_room' ]
+    });
+    socket.on('sendmsg', function(data){
+        console.log('server: ' + data);
+        messageHistory.push(String(data));
+        socket.broadcast.emit('updateHeader', data);
+    });
+});
